ea39095452993021490a002874821ef46ec70913
Thomas Basham <Bashamtg@gmail.com>
2022-10-06 19:53:46 -0700

    reset password route working

--- pages/password-reset.js
+++ pages/password-reset.js
@@ -14,61 +14,17 @@ function PasswordReset() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const notification = toast.loading("Changing Password");
-
     try {
-      // if the user doesn't have accesstoken
-      if (!hash) {
-        return toast.error("Sorry, Invalid token", {
-          id: notification,
-        });
-      } else if (hash) {
-        const hashArr = hash
-          .substring(1)
-          .split("&")
-          .map((param) => param.split("="));
+      //   now we will change the password
+      const { error } = await supabase.auth.updateUser({
+        password: password,
+      });
 
-        let type;
-        let accessToken;
-        for (const [key, value] of hashArr) {
-          if (key === "type") {
-            type = value;
-          } else if (key === "access_token") {
-            accessToken = value;
-          }
-        }
-
-        if (
-          type !== "recovery" ||
-          !accessToken ||
-          typeof accessToken === "object"
-        ) {
-          toast.error("Invalid access token or type", {
-            id: notification,
-          });
-          return;
-        }
-
-        //   now we will change the password
-        const { error } = await supabase.auth.api.updateUser(accessToken, {
-          password: password,
-        });
-
-        if (error) {
-          toast.error(error.message, {
-            id: notification,
-          });
-        } else if (!error) {
-          toast.success("Password Changed", {
-            id: notification,
-          });
-        }
+      if (error) {
+        throw error;
       }
     } catch (error) {
       console.log(error);
-      toast.error("Sorry Error occured", {
-        id: notification,
-      });
     }
   };
 
