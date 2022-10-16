053c343a3abfc53084f09e0ebcd5130e4f285a93
Thomas Basham <Bashamtg@gmail.com>
2022-10-16 12:25:00 -0700

    removed unused import

--- pages/password-reset.js
+++ pages/password-reset.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import toast from "react-hot-toast";
 import { supabase } from "../utils/supabaseClient";
 import { useRouter } from "next/router";
 import { useAuth } from "../contexts/auth";
