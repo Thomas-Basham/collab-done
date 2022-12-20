1ac79d4861b0ec2113c42f133ff38dff2ad71fca
Thomas Basham <Bashamtg@gmail.com>
2022-12-20 12:06:58 -0800

    cleaned up code

--- pages/pr/[id].js
+++ pages/pr/[id].js
@@ -15,53 +15,52 @@ export default function ForeignUserProfilePage() {
 
   useEffect(() => {
     if (!socials) {
-
       handleGetSocials();
     }
   });
 
   const handleGetSocials = async () => {
     const { id } = router.query;
-    console.log({id})
+    console.log({ id });
     let data = await getSocials(id, 0);
     setSocials(data);
   };
 
   return (
     <>
-    <Container >
-      <button className="rounded bio" onClick={() => router.back()}>
-        <RiArrowGoBackFill />
-      </button>
-      <br></br>
+      <Container>
+        <button className="rounded bio" onClick={() => router.back()}>
+          <RiArrowGoBackFill />
+        </button>
+        <br></br>
 
-      <div className="bio">
-        <div className="socials-container">
-          <Socials
-            data={socials}
-            currentUser={session?.user}
-            username={username}
-          />
-        </div>
-        <h1>{socials?.username}</h1>
-        <a
-          href={`https://${socials?.website}`}
-          target="_blank"
-          rel="noreferrer"
-        >
-          <h6>{socials?.website}</h6>
-        </a>
-        <Avatar url={socials?.avatar_url} size={150} />
-        <div>
+        <div className="bio">
+          <div className="socials-container">
+            <Socials
+              data={socials}
+              currentUser={session?.user}
+              username={username}
+            />
+          </div>
+          <h1>{socials?.username}</h1>
+          <a
+            href={`https://${socials?.website}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <h6>{socials?.website}</h6>
+          </a>
+          <Avatar url={socials?.avatar_url} size={150} />
           <div>
-            <br></br>
-            <label htmlFor="bio">Bio</label>
-            <p className="bio" style={{ width: "95%" }} type="text">
-              {socials?.bio}
-            </p>
+            <div>
+              <br></br>
+              <label htmlFor="bio">Bio</label>
+              <p className="bio" style={{ width: "95%" }} type="text">
+                {socials?.bio}
+              </p>
+            </div>
           </div>
         </div>
-      </div>
       </Container>
     </>
   );
